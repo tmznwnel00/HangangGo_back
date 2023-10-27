705108161543aee977f7cf6d86a347df2602d84f
tmznwnel00 <tmznwnel00@gmail.com>
2023-10-27 22:02:32 +0900

    feat: get image from client

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from firebase_admin import storage
 from flaskr.db import bucket
+import sys
+import os
 
 app = Flask(__name__)
 
@@ -12,14 +14,21 @@ def hello():
 @app.route('/api/images', methods=['POST'])
 def upload_image():
     # save image in firestore, and call function for classify speicies
-    params = request.get_json()
-    # image_path = params['iamge_path']
-    image_path = 'gs://hanganggo-88a45.appspot.com/sample.webp'
-    
-    blob = bucket.blob(image_path)
+    image = request.files["image"]
+    _, image_extension = os.path.splitext(image.filename)
+
+    image.save(os.path.join(os.getcwd() + '/../images', f'hi{image_extension}'))
+    image_path = os.getcwd() + '/../images/' + f'hi{image_extension}'
+    classify_from_image(image_path)
     
     return 'Hello, My First Flask!'
 
-def classify_from_image(image):
+def classify_from_image(image_path):
+    sys.argv = [image_path]
+    
+    with open("../SpeciesClassification/classify_images.py", "r") as classify:
+        code = classify.read()
+    
+    exec(code)
     
     return 'Hello, My First Flask!'
