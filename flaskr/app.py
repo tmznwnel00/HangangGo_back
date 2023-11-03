9cf4fd42369474cf0adc991fa22505a020636ae1
tmznwnel00 <tmznwnel00@gmail.com>
2023-11-03 16:52:57 +0900

    feat: image upload to storage

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,42 +3,68 @@ from firebase_admin import storage
 from flaskr.db import bucket, species_map, collection
 import sys
 import os
+from datetime import datetime
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 # from SpeciesClassification import classify_images
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Hello, My First Flask!'
+    return "Hello, My First Flask!"
 
 
-@app.route('/api/images', methods=['POST'])
+@app.route("/api/images", methods=["POST"])
 def upload_image():
     # save image in firestore, and call function for classify speicies
     image = request.files["image"]
     _, image_extension = os.path.splitext(image.filename)
-
-    image.save(os.path.join(os.getcwd() + '/../images', f'user1_2{image_extension}'))
-    image_path = os.getcwd() + '/../images/' + f'user1_2{image_extension}'
-    classify_from_image(image_path)
+    current_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    image.save(os.path.join(os.getcwd() + "/../images", f"user1_{current_time}{image_extension}"))
+    image_path = os.getcwd() + "/../images/" + f"user1_{current_time}{image_extension}"
     
-    return 'Hello, My First Flask!'
+    result = classify_from_image(image_path)
+    
+    
+    collection_data = {
+            "user_id": 1,
+            "category": "식물",
+            "korean_name": result["name"],
+            "imgLink": "gs://hanganggo-88a45.appspot.com/Corydalis_incisa_pers.jpeg",
+            "remark": "기후변화 생물지표종",
+            "lat": 25,
+            "lng": 126,
+            "hangang_alphabet": "Q",
+            "description": "기후변화 생물지표종으로 긴 타원형의 뿌리에서 여러 대의 줄기가 나와 20-50cm까지 자란다. 잎은 3-8cm로 길고 작은 잎이 3장씩 2번 갈라져 나온다. 그늘지고 축축한 땅에서 자라고 5월에 홍자색 꽃을 피우고 4-12cm까지 자란다.",
+            "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+    imageBlob = bucket.blob("/")
+    imageBlob = bucket.blob(result["name"])
+    # imageBlob.upload_from_filename(image_path)
+    imageBlob.upload_from_string(image.read(), content_type=image.content_type)
+    
+    return "Hello, My First Flask!"
+
 
 def classify_from_image(image_path):
-
     result = classify_images.run_model(image_path)
     print(result)
-    
-    data = {
-        "result" : result[0][2]
-    }
-    
+
+    if float(result[0][3]) < 0.6:
+        data = {"name": "Can not classify"}
+    else:
+        # should convert common name to korean name
+        data = {"name": result[0][2]}
+
     return data
 
-@app.route('/api/maps', methods=['GET'])
+
+@app.route("/api/maps", methods=["GET"])
 def get_species_map():
-    
     query = species_map.get()
     val = query.values()
     result = []
@@ -50,15 +76,14 @@ def get_species_map():
         if specie["hangang_alphabet"] in ["S", "R", "Q", "P", "O", "N", "M2", "M1"]:
             result.append(specie)
 
-    data = {
-        "species" : result
-    }
-    
+    data = {"species": result}
+
     return data
 
-@app.route('/api/collections', methods=['GET'])
+
+@app.route("/api/collections", methods=["GET"])
 def get_user_collection():
     user = request.args.get("user_id")
-    query = collection.order_by_child('user_id').equal_to("1")
+    query = collection.order_by_child("user_id").equal_to("1")
     result = query.get()
     return result
