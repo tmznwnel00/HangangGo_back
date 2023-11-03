19dfa9020f40735174fe0d3216017edaefe606ce
tmznwnel00 <tmznwnel00@gmail.com>
2023-11-04 01:19:28 +0900

    fix: delete updating collection table

--- flaskr/db.py
+++ flaskr/db.py
@@ -130,15 +130,28 @@ data = {
 }
 
 data2 = {
-    "category": "식물",
-    "korean_name": "줄장지뱀",
-    "imgLink": "gs://hanganggo-88a45.appspot.com/Takydromus_wolteri.jpeg",
-    "remark": "서울시 보호종",
-    "location": "서울특별시 영등포구 여의공원로 68",
-    "hangang_alphabet": "R",
-    "description" : "서울시 보호종으로 잎은 달걀 혹은 콩팥처럼 생겼다. 크기는 보통 길이 1.5-2.5cm, 너비 2-3cm로 끝은 둥글고 밑은 심장형이며 가장자리에 둔한 톱니가 있다. 꽃은4~5월에 연한 보라색으로 핀다.",
+    "user_id": 1,
+    "category": "자주괴불주머니",
+    "korean_name": "식물",
+    "imgLink": "gs://hanganggo-88a45.appspot.com/Corydalis_incisa_pers.jpeg",
+    "remark": "기후변화 생물지표종",
+    "lat": 25,
+    "lng": 126,
+    "hangang_alphabet": "Q",
+    "description" : "기후변화 생물지표종으로 긴 타원형의 뿌리에서 여러 대의 줄기가 나와 20-50cm까지 자란다. 잎은 3-8cm로 길고 작은 잎이 3장씩 2번 갈라져 나온다. 그늘지고 축축한 땅에서 자라고 5월에 홍자색 꽃을 피우고 4-12cm까지 자란다.",
     "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
 }
 
-collection.push(data2)
+# data3 = {
+#             "category": "양서파충류",
+#             "createdAt": "2023-11-03 00:24:33",
+#             "hangang_alphabet": "R",
+#             "imgLink": "gs://hanganggo-88a45.appspot.com/Takydromus_wolteri.jpeg",
+#             "lat": 37.56624727206846,
+#             "lng": 126.89434875328772,
+#             "remark": "우점",
+#             "speciesName": "줄장지뱀",
+#             "updatedAt": "2023-11-03 00:24:33"
+#         }
+# species_map.update(data3)
